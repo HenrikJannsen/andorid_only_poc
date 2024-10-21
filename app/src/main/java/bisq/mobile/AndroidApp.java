fac665d371b79e3a9c5e2d16e311db69dd3a44de
HenrikJannsen <boilingfrog@gmx.com>
2024-10-21 22:30:26 +0700

    Log num connections

--- app/src/main/java/bisq/mobile/AndroidApp.java
+++ app/src/main/java/bisq/mobile/AndroidApp.java
@@ -2,12 +2,17 @@ package bisq.mobile;
 
 
 import java.security.KeyPair;
+import java.util.Optional;
 
 import bisq.common.encoding.Hex;
 import bisq.common.observable.Observable;
+import bisq.common.timer.Scheduler;
 import bisq.network.NetworkService;
 import bisq.network.common.Address;
 import bisq.network.common.TransportType;
+import bisq.network.p2p.ServiceNode;
+import bisq.network.p2p.node.Node;
+import bisq.network.p2p.services.peer_group.PeerGroupManager;
 import bisq.security.keys.KeyBundleService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,7 +25,7 @@ public class AndroidApp {
 
     public AndroidApp(String userDataDir, boolean isRunningInAndroidEmulator) {
         Address.setIsRunningInAndroidEmulator(isRunningInAndroidEmulator);
-        androidApplicationService= AndroidApplicationService.getInitializedInstance(userDataDir);
+        androidApplicationService = AndroidApplicationService.getInitializedInstance(userDataDir);
         KeyBundleService keyBundleService = androidApplicationService.getSecurityService().getKeyBundleService();
         defaultKeyId = keyBundleService.getDefaultKeyId();
         keyPair = keyBundleService.getOrCreateKeyBundle(defaultKeyId).getKeyPair();
@@ -30,6 +35,16 @@ public class AndroidApp {
 
         NetworkService networkService = androidApplicationService.getNetworkService();
         networkService.getDefaultNodeStateByTransportType().get(TransportType.CLEAR)
-                .addObserver(state-> logMessage.set(logMessage.get() + "Network state: " + state.toString() + "\n"));
+                .addObserver(state -> logMessage.set(logMessage.get() + "Network state: " + state.toString() + "\n"));
+
+        ServiceNode serviceNode = networkService.getServiceNodesByTransport().findServiceNode(TransportType.CLEAR).orElseThrow();
+        Node defaultNode = serviceNode.getDefaultNode();
+        PeerGroupManager peerGroupManager = serviceNode.getPeerGroupManager().orElseThrow();
+        var peerGroupService = peerGroupManager.getPeerGroupService();
+
+        Scheduler.run(() -> {
+            long numConnections = peerGroupService.getAllConnectedPeers(defaultNode).count();
+            logMessage.set(logMessage.get() + "numConnections: " + numConnections + "\n");
+        }).periodically(1000);
     }
 }
